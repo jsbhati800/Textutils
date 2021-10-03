a92e6c451a2608bd3af2f47377c287062a3651f1
Jai singh <bhatijaisingh678@gmail.com>
2021-10-03 13:47:03 +0530

    first commit

---
+++ textutills/views.py
@@ -0,0 +1,55 @@
+# I have created this file- Jsb
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    # get the text
+    djtext=request.POST.get('text','default')
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+
+    if (removepunc=="on") :
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed=""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed + char
+        params={'purpose':'','analyzed_text':analyzed}
+        djtext=analyzed
+
+    if (fullcaps=="on"):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed + char.upper()
+        params = {'purpose': '', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose': '', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (extraspaceremover=="on"):
+        analyzed = ""
+        for index,char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': '', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (removepunc!="on" and fullcaps!="on" and newlineremover != "on" and extraspaceremover!="on"):
+        return HttpResponse("Please select any option & try again")
+
+    return render(request, 'analyze.html', params)
+
+
+
+
